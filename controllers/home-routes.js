13c779dc69c7c0fabba45e0785aafbf6b9fb1a7f
Padredilg <padredilg@knights.ucf.edu>
2021-09-27 12:08:19 -0400

    implementing sorting by votes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,57 +3,7 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', (req, res) => {
-    console.log(req.session);
-    Post.findAll({
-        attributes: [
-            'id',
-            'content',
-            'title',
-            'created_at',
-            'user_id',
-            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-        ],
-        include: [
-            {
-                model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
-    })
-    .then(dbPostData => {
-        // pass a single post object into the homepage template
-        const posts = dbPostData.map(post => {
-            post = post.get({ plain: true })
 
-            if(post.user_id === req.session.user_id){
-                post.edit = true;
-            }
-            else{
-                post.edit = false;
-            }
-            return post;
-        });
-
-        res.render('homepage', {
-            posts,
-            loggedIn: req.session.loggedIn,
-            username: req.session.username
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
 
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
@@ -126,6 +76,64 @@ router.get('/post/:id', withAuth, (req, res) => {
             console.log(err);
             res.status(500).json(err);
       });
+});
+
+router.get('/:option?', (req, res) => {
+    console.log(req.session);
+    let orderBy = [['created_at', 'DESC']];
+    if (req.params.option === 'most-popular') {
+        orderBy = [[[sequelize.literal('vote_count', 'ASC')]]]
+    }
+
+    Post.findAll({
+        attributes: [
+            'id',
+            'content',
+            'title',
+            'created_at',
+            'user_id',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ],
+        order: orderBy
+    })
+    .then(dbPostData => {
+        // pass a single post object into the homepage template
+        const posts = dbPostData.map(post => {
+            post = post.get({ plain: true })
+
+            if(post.user_id === req.session.user_id){
+                post.edit = true;
+            }
+            else{
+                post.edit = false;
+            }
+            return post;
+        });
+
+        res.render('homepage', {
+            posts,
+            loggedIn: req.session.loggedIn,
+            username: req.session.username
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
